e076881d61d35ad3070ca94cb0f428dbb54be90c
Robert Kim <robertjoonkim@gmail.com>
2014-04-14 00:16:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+FAT HACK GENIUS app
+===========================
+
+## Description ##
+
+- A health and wellness app designed to help you lose weight, educate on how to maintain long-term results, and make you healthier as a result.  
+
+## Built with the help of: ##
+
+- the FoodEssentials API (managed by Mashery)
+- Twitter Bootstrap 
+- Devise
+- CanCan
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FatHackGenius::Application.config.secret_token = 'c57ba22fe578fa46d5ffa59d1677e28a571d970df167933ce8452355aa876571bb99987f43b0ee74ad12f49f879805e8b0fa0522f14a9c109a8d4896b3553352'
